5009002192f632481adf83a81e2162e4e0edaf9e
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-04-01 22:47:24 +1000

    Minor simplification to finding second lowest score

--- python/02-basic-data-types/03_nested_lists.py
+++ python/02-basic-data-types/03_nested_lists.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
 
     sorted_grades = sorted(grades, key=lambda x: x['score'])
 
-    second_lowest_score = [grade['score'] for grade in sorted_grades if grade['score'] > sorted_grades[0]['score']][0]
+    second_lowest_score = list(filter(lambda grade: grade['score'] > sorted_grades[0]['score'], sorted_grades))[0]
 
     for name in sorted([grade['name'] for grade in sorted_grades if grade['score'] == second_lowest_score]):
         print(name)
