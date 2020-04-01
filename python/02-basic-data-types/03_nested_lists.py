61d3d34042ba85ca98bfc0fec9ae03a0f0b1a2dc
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-04-01 20:47:19 +1000

    Initial attempt at nested lists problem

---
+++ python/02-basic-data-types/03_nested_lists.py
@@ -0,0 +1,17 @@
+# https://www.hackerrank.com/challenges/nested-list/problem
+
+if __name__ == '__main__':
+    grades = []
+
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+
+        grades.append({'name': name, 'score': score})
+
+    sorted_grades = sorted(grades, key=lambda x: x['score'])
+
+    second_lowest_score = [grade['score'] for grade in sorted_grades if grade['score'] > sorted_grades[0]['score']][0]
+
+    for name in sorted([grade['name'] for grade in sorted_grades if grade['score'] == second_lowest_score]):
+        print(name)
