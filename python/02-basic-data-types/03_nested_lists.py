e16a8fbcbe27f55600f065eb438252ebcfa400e7
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-04-05 00:02:14 +1000

    Solve some more challenges

--- python/02-basic-data-types/03_nested_lists.py
+++ python/02-basic-data-types/03_nested_lists.py
@@ -11,7 +11,8 @@ if __name__ == '__main__':
 
     sorted_grades = sorted(grades, key=lambda x: x['score'])
 
-    second_lowest_score = list(filter(lambda grade: grade['score'] > sorted_grades[0]['score'], sorted_grades))[0]
+    lowest_score = sorted_grades[0]['score']
+    second_lowest_score = list(filter(lambda grade: grade['score'] > lowest_score, sorted_grades))[0]
 
     for name in sorted([grade['name'] for grade in sorted_grades if grade['score'] == second_lowest_score]):
         print(name)
---
+++ python/03-built-ins/05_python_evaluation.py
@@ -0,0 +1,3 @@
+# https://www.hackerrank.com/challenges/python-eval/problem
+
+eval(input())
