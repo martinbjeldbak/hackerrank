009880f0488c6cca0e7f90af5b043e6510aeffeb
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-04-05 15:05:25 +1000

    Learn about python sorting and use sorting function

--- python/03-built-ins/05_ginorts.py
+++ python/03-built-ins/05_ginorts.py
@@ -1,14 +1,16 @@
 # https://www.hackerrank.com/challenges/ginorts/problem
 S = input()
 
-lowercase_letters = [s for s in S if s.islower()]
-uppercase_letters = [s for s in S if s.isupper()]
-digits = [s for s in S if s.isdigit()]
-odd_digits = "".join([digit for digit in digits if (int(digit) % 2) != 0])
-even_digits = "".join([digit for digit in digits if (int(digit) % 2) == 0])
+def sortRule(x):
+    if x.islower():
+        return (0, x)
+    elif x.isupper():
+        return (1, x)
+    elif x.isdigit() and int(x) % 2 != 0:
+        return (2, x)
+    else:
+        return (3, x)
 
-print("".join(
-    sorted(lowercase_letters) +
-    sorted(uppercase_letters) +
-    sorted(odd_digits, key=lambda d: int(d)) +
-    sorted(even_digits, key=lambda d: int(d))))
+
+print("".join(sorted(S, key=sortRule)))
+
