340fe155370aedf38451c65fecb12059c36aa6d6
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-05-10 23:12:55 +1000

    Implement logic to fix failing spec for case

--- java/src/main/java/com/martinbjeldbak/javastdinandstdoutii/Solution.java
+++ java/src/main/java/com/martinbjeldbak/javastdinandstdoutii/Solution.java
@@ -3,7 +3,6 @@ package com.martinbjeldbak.javastdinandstdoutii;
 import java.util.Scanner;
 
 public class Solution {
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int i = scan.nextInt();
