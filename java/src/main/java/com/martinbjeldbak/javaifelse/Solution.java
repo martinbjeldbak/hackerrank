48f1837b8ce5a24f367f1d331cbc70ab9031848b
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-05-10 17:34:01 +1000

    Initial solution

---
+++ java/src/main/java/com/martinbjeldbak/javaifelse/Solution.java
@@ -0,0 +1,29 @@
+package com.martinbjeldbak.javaifelse;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        scanner.close();
+
+        if(N % 2 == 1) { // odd
+            System.out.println("Weird");
+        }
+        else { // even
+            if(N > 1 && N < 6) {
+                System.out.println("Not Weird");
+            }
+            else if(N > 5 && N < 21) {
+                System.out.println("Weird");
+            }
+            else {
+                System.out.println("Not Weird");
+            }
+
+        }
+    }
+}
+
