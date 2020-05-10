7c8aadd1c1c9ed1e6a5a3deb2bbd60a30f0887ce
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-05-10 22:04:51 +1000

    Initial failing spec for std input and output 2

---
+++ java/src/test/java/com/martinbjeldbak/javastdinandstdoutii/SolutionTest.java
@@ -0,0 +1,47 @@
+package com.martinbjeldbak.javastdinandstdoutii;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    private final InputStream originalIn = System.in;
+    private final String[] defaultInput = new String[]{};
+    private ByteArrayInputStream inContent;
+    private String input;
+
+    @BeforeEach
+    void init() {
+        System.setOut(new PrintStream(outContent));
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(originalOut);
+        System.setIn(originalIn);
+    }
+
+
+    @Test
+    void testCase1() {
+        input = "3\n1,01\nHello, Java.";
+
+        inContent = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inContent);
+
+        Solution.main(defaultInput);
+
+        assertEquals("String: Hello, Java.\nDouble: 1.01\nInt: 3\n", outContent.toString());
+    }
+}
