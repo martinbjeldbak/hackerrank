c5f0a7d5caebf75b1131b05210fdbf690ad18860
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-05-10 16:40:28 +1000

    Rename first solution spec to match updated package

--- java/src/test/java/com/martinbjeldbak/welcometojava/SolutionTest.java
+++ java/src/test/java/com/martinbjeldbak/welcometojava/SolutionTest.java
@@ -1,8 +1,6 @@
-package com.martinbjeldbak;
+package com.martinbjeldbak.welcometojava;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import com.martinbjeldbak.welcometojava.Solution;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
