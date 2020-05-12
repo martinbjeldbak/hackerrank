68be1914c2f2b536223743a2af336feb1ade4baa
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-05-12 15:47:43 +1000

    Reformat implementation to match problem requirements

--- java/src/main/java/com/martinbjeldbak/javastaticinitializerblock/Solution.java
+++ java/src/main/java/com/martinbjeldbak/javastaticinitializerblock/Solution.java
@@ -21,14 +21,19 @@ public class Solution {
         scanner.close();
 
         flag = B > 0 && H > 0;
+
+        if(!flag)
+            try {
+                throw new Exception("Breadth and height must be positive");
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            }
     }
 
     public static void main(String[] args) {
         if (flag) {
             int area = B * H;
             System.out.print(area);
-        } else {
-            System.out.println("java.lang.Exception: Breadth and height must be positive");
         }
     }
 }
