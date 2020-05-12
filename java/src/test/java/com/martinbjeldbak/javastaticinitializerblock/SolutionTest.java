5f23c96b4eb51d4585c108897e6debe699946091
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-05-12 15:42:52 +1000

    Implement failing spec for Java Static Initializer Block problem

---
+++ java/src/test/java/com/martinbjeldbak/javastaticinitializerblock/SolutionTest.java
@@ -0,0 +1,60 @@
+package com.martinbjeldbak.javastaticinitializerblock;
+
+import com.martinbjeldbak.javastaticinitializerblock.Solution;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    private final InputStream originalIn = System.in;
+    private final String[] defaultInput = new String[]{};
+    private ByteArrayInputStream inContent;
+    private String input;
+
+    @BeforeEach
+    void init() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(originalOut);
+        System.setIn(originalIn);
+    }
+
+    @Test
+    void testCase1() {
+        input = "1\n" +
+                "3\n";
+
+        inContent = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inContent);
+
+        Solution.main(defaultInput);
+
+        assertEquals("3", outContent.toString());
+    }
+
+    @Test
+    void testCase2() {
+        input = "-1\n" +
+                "2\n";
+
+        inContent = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inContent);
+
+        Solution.main(defaultInput);
+
+        assertEquals("java.lang.Exception: Breadth and height must be positive", outContent.toString());
+    }
+}
