4b927e73a73f107043250d65d0dfceae1edd84b8
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-05-12 16:13:31 +1000

    Write initial failing spec for Java Date and Time problem

---
+++ java/src/test/java/com/martinbjeldbak/javadateandtime/SolutionTest.java
@@ -0,0 +1,47 @@
+package com.martinbjeldbak.javadateandtime;
+
+import com.martinbjeldbak.javadateandtime.Solution;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    private final InputStream originalIn = System.in;
+    private final String[] defaultInput = new String[]{};
+    private ByteArrayInputStream inContent;
+    private String input;
+
+    @BeforeEach
+    void init() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(originalOut);
+        System.setIn(originalIn);
+    }
+
+    @Test
+    void testCase1() {
+        input = "08 05 2015\n";
+
+        inContent = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inContent);
+
+        Solution.main(defaultInput);
+        assertEquals("WEDNESDAY", outContent.toString());
+    }
+}
+
+
