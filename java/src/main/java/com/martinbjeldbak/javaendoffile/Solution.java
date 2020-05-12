1868c2035eb6489e1b86f93b122f8a03af7d83ca
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-05-12 15:18:05 +1000

    Simplify solution with #hasNext method

--- java/src/main/java/com/martinbjeldbak/javaendoffile/Solution.java
+++ java/src/main/java/com/martinbjeldbak/javaendoffile/Solution.java
@@ -1,6 +1,5 @@
 package com.martinbjeldbak.javaendoffile;
 
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 // https://www.hackerrank.com/challenges/java-end-of-file/problem
@@ -8,16 +7,10 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Integer i = 1;
-        String line;
+        int i = 1;
 
-
-        try {
-            while((line = scanner.nextLine()) != "") {
-                System.out.println(i + " " + line);
-                i++;
-            }
-        } catch(NoSuchElementException ignored) {
+        while(scanner.hasNext()) {
+            System.out.println(i++ + " " + scanner.nextLine());
         }
     }
 }
