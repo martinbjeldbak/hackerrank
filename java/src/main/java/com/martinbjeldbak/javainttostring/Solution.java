9122db99672e690b28b1a0497d2737767350b9c1
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-07-06 22:56:20 +1000

    Get maven tests passing

--- java/src/main/java/com/martinbjeldbak/javainttostring/Solution.java
+++ java/src/main/java/com/martinbjeldbak/javainttostring/Solution.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Solution {
     public static void main(String[] args) {
 
-        DoNotTerminate.forbidExit();
+        //DoNotTerminate.forbidExit();
 
         try {
             Scanner in = new Scanner(System.in);
