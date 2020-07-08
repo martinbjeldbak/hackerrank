17e9ce203603a75d6356cf55b5a6720ab2b71e6f
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-07-08 22:42:30 +1000

    Implementation with set

--- java/src/main/java/com/martinbjeldbak/FindMissing.java
+++ java/src/main/java/com/martinbjeldbak/FindMissing.java
@@ -1,10 +1,20 @@
 package com.martinbjeldbak;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class FindMissing {
     public FindMissing() {
     }
 
+    // [4, 12, 9, 5, 6]
+    // [4, 9, 12, 6]
+    // 5
     public int call(int[] fullSet, int[] partialSet) {
-        return 0;
+        Set<Integer> fullSetSet = Arrays.stream(fullSet).boxed().collect(Collectors.toSet());
+        Set<Integer> partialSetSet = Arrays.stream(partialSet).boxed().collect(Collectors.toSet());
+
+        fullSetSet.removeAll(partialSetSet);
+        return fullSetSet.toArray(new Integer[1])[0];
     }
 }
