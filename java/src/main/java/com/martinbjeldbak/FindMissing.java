5e27f94c7238a81a15735a79cdb3dff0af153f5f
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-07-08 23:09:14 +1000

    Initial implementation of array-based solution

--- java/src/main/java/com/martinbjeldbak/FindMissing.java
+++ java/src/main/java/com/martinbjeldbak/FindMissing.java
@@ -23,4 +23,25 @@ public class FindMissing {
             return missing[0];
         }
     }
+
+    // [4, 12, 9, 5, 6]
+    // [4, 9, 12, 6]
+    // 5
+    public int callArrayImplementation(int[] fullSet, int[] partialSet) {
+        for(int i = 0; i < fullSet.length; i++) {
+            if(!containsElement(partialSet, fullSet[i])) {
+                return fullSet[i];
+            }
+        }
+        return -1;
+    }
+
+    public boolean containsElement(int[] array, int element) {
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] == element) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
