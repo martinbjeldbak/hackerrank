e29ae2ef1a26f5fd40b9168ed6da254d1a6aa502
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-07-08 22:49:53 +1000

    Add failing spec for array implementation

--- java/src/test/java/com/martinbjeldbak/FindMissingTest.java
+++ java/src/test/java/com/martinbjeldbak/FindMissingTest.java
@@ -6,16 +6,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FindMissingTest {
     @Test
-    void testExample1() {
+    void testSetImplementation() {
         FindMissing subject = new FindMissing();
 
         assertEquals(5, subject.call(new int[] {4, 12, 9, 5, 6 }, new int[] {4, 9, 12, 6}));
     }
 
     @Test
-    void testWithoutMatch() {
+    void testSetImplementationWithoutMatch() {
         FindMissing subject = new FindMissing();
 
         assertEquals(-1, subject.call(new int[] {4, 12, 9, 6 }, new int[] {4, 9, 12, 6}));
+    }
+
+    @Test
+    void testArrayImplementation() {
+        FindMissing subject = new FindMissing();
+
+        assertEquals(5, subject.callArrayImplementation(new int[] {4, 12, 9, 5, 6 }, new int[] {4, 9, 12, 6}));
     }
 }
